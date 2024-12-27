14245d567684c6ec744a698e1318e8e717034191
Mariam-510 <mariamashrafamin@gmail.com>
2024-12-27 20:47:33 +0200

    update query

--- Query/CreateTables.sql
+++ Query/CreateTables.sql
@@ -1,6 +1,59 @@
 Use Hospital
 
 --------------------------------------------------------------------------------------------------
+--Department table
+create table Department
+(
+    Id int identity(100,1) primary key,
+    Name nvarchar(100) not null
+)
+
+--------------------------------------------------------------------------------------------------
+--Employee table
+create table Employee 
+(
+    Id int identity(1,1) primary key,
+    EmpType char(1) not null check (EmpType in ('D', 'N', 'R')), -- restrict to D, N, or R
+    Fname nvarchar(50) not null,
+    Lname nvarchar(50) not null,
+    DOB date not null,
+    Phone nvarchar(15),
+    Gender char(1) not null check (Gender in ('M', 'F')),    -- restrict to M, F
+    Street nvarchar(100),
+    City nvarchar(50) default 'Cairo',
+    State nvarchar(50) default 'Egypt', --edit
+    DeptID int foreign key references Department(id)
+)
+
+--------------------------------------------------------------------------------------------------
+--Doctor table
+create table Doctor (
+    Id int primary key foreign key references Employee(id),
+    Speciality nvarchar(100) not null
+)
+
+--------------------------------------------------------------------------------------------------
+--Nurse table
+create table Nurse (
+    Id int primary key foreign key references Employee(id)
+)
+
+--Receptionist table
+create table Receptionist (
+    Id int primary key foreign key references Employee(id)
+);
+
+--------------------------------------------------------------------------------------------------
+--Room Table
+Create Table Room 
+(
+ Num int primary key identity (100,1),
+ Availblitiy char(1) not null default 'Y', --num or char
+ Capacity int,
+ NId int foreign key references Nurse(Id)
+)
+
+--------------------------------------------------------------------------------------------------
 --Patient Table
 create table Patient
 (
@@ -22,8 +75,7 @@ create table InPatient
 (
 Id int primary key foreign key references Patient(Id),
 DischargeDate datetime2 default getdate(),
-RoNum int 
---foreign key references Room(Num)
+RoNum int foreign key references Room(Num)
 )
 
 --------------------------------------------------------------------------------------------------
@@ -38,14 +90,90 @@ Id int primary key foreign key references Patient(Id),
 create table Appointment
 (
 Id int primary key identity(1000,1),
-Date datetime2 default getdate(),
+Date datetime2 not null default getdate(),
 Note varchar(100),
-DId int,
---foreign key references Doctor(Id)
+DId int foreign key references Doctor(Id),
 PId int foreign key references Patient(Id),
-RId int
---foreign key references Receptionist(Id)
+RId int foreign key references Receptionist(Id)
 )
 
 --------------------------------------------------------------------------------------------------
+--Drug Table
+create table Drug
+(
+  code int primary key,
+  RecDosage varchar(50)
+)
 
+--------------------------------------------------------------------------------------------------
+--DrugBrand Table
+create Table DrugBrand
+(
+    DrugCode int foreign key references Drug(code),
+    BrandName varchar(50),
+	constraint Drug_Brand_pk primary Key(DrugCode, BrandName)
+)
+
+--------------------------------------------------------------------------------------------------
+--Prescription Table
+create table Prescription
+(
+  Id int primary key identity (10,1),
+  Date datetime2 not null default getDate(), --edit
+  DID int foreign key references Doctor(Id),
+  PID int foreign key references Patient(Id) unique --edit
+)
+
+--------------------------------------------------------------------------------------------------
+--PrescriptionDrug Table
+create Table PrescriptionDrug 
+(
+    PreID int foreign key references Prescription(Id),
+	DrugCode int foreign key references Drug(code),
+	Dosage varchar(50),
+	constraint prescription_Drug_pk primary Key(PreID, DrugCode)
+)
+
+--------------------------------------------------------------------------------------------------
+--Report Table
+Create Table Report
+(
+ Id int primary key identity (10,1),
+ Disease varchar(100) not null,
+ Symptom  varchar(100) not null,
+ Diagnosis varchar(100),
+ PId int foreign key references Patient(Id) unique, --edit
+ DID int foreign key references Doctor(Id)
+)
+
+--------------------------------------------------------------------------------------------------
+--Bill Table
+Create Table Bill
+(
+ Id int primary key identity (10,1),
+ Date datetime2 not null default getDate(),
+ Amount decimal(10,2) not null, --make it money
+ PId int foreign key references Patient(Id) unique, --edit
+ RID int foreign key references Receptionist(Id)
+)
+
+--------------------------------------------------------------------------------------------------
+--ExaminePatient Table
+create Table ExaminePatient 
+(
+    DID int foreign key references Doctor(Id),
+    PID int foreign key references Patient(Id),
+	constraint E_Patient_pk primary Key(DID, PID)
+)
+
+--------------------------------------------------------------------------------------------------
+--GivenDrug Table
+create Table GivenDrug 
+(
+    Dosage varchar(50) not null,
+	GivenDate datetime2 default getdate(),
+    DrugCode int foreign key references Drug(code),
+	Inpid int foreign key references InPatient(Id),
+    NID int foreign key references Nurse(Id),
+	constraint Give_Drug_pk primary Key(GivenDate, DrugCode, Inpid, NID)
+)
--- Query/InsertValues.sql
+++ Query/InsertValues.sql
@@ -1,88 +1,250 @@
 use Hospital
 
--- Insert OutPatient
-declare @InsertedPatientIdTable1 table (Id int)
-
-insert into Patient (PType, FName, DOB, Gender)
-output inserted.Id into @InsertedPatientIdTable1
-values ('O', 'Sara', '1990-07-08', 'F')
-
-declare @InsertedPatientId1 int
-select @InsertedPatientId1 = Id from @InsertedPatientIdTable1
-
-insert into OutPatient (Id)
-values (@InsertedPatientId1)
+--------------------------------------------------------------------------------------------------
+--Insert records into Department table
+insert into Department (Name) 
+values ('Cardiology'), ('Neurology'), ('Pediatrics'), ('General Surgery'), ('Orthopedics'), ('Secretarial');
 
 --------------------------------------------------------------------------------------------------
--- Insert OutPatient
-declare @InsertedPatientIdTable2 table (Id int)
+--Insert records into Doctor table
+declare @insertedDocIDt1 TABLE (id INT, Speciality NVARCHAR(100));
+insert into Employee (EmpType, Fname, Lname, DOB, Phone, Gender, Street, City, State, DeptID)
+output inserted.id, case 
+                       when inserted.Fname = 'Ahmed' then 'Cardiologist'
+                       when inserted.Fname = 'Khaled' then 'Neurologist'
+                       when inserted.Fname = 'hala' then 'Pediatrician'
+					   when inserted.Fname = 'Rana' then 'General Surgeon'
+					   when inserted.Fname = 'Mostafa' then 'Orthopedic Surgeon	'
+                   end into @insertedDocIDt1
+values
+    ('D', 'Ahmed', 'Ali', '1985-05-12', '0123456789', 'M', 'Street 1', 'Giza', 'Egypt', 100),
+    ('D', 'Khaled', 'Mahmoud', '1980-11-05', '0103456789', 'M', 'Street 2', default, default, 102),
+    ('D', 'hala', 'Adel', '1990-07-20', '0113756780', 'F', 'Street 33', 'Alexandria', 'Egypt', 103),
+	('D', 'Rana', 'Samir', '1996-01-01', '0103950789', 'F', 'Street 51', default, default, 104),
+    ('D', 'Mostafa', 'Abdallah', '1989-06-05', '0123556787', 'M', 'Street 9', default, default, 101)
 
-insert into Patient (FName, LName, DOB, Gender, Street)
-output inserted.Id into @InsertedPatientIdTable2
-values ('Ahmed', 'Mohamed', '2000-05-20', 'M', 'Broadway')
-
-declare @InsertedPatientId2 int
-select @InsertedPatientId2 = Id from @InsertedPatientIdTable2
-
-insert into OutPatient (Id)
-values (@InsertedPatientId2)
+insert into Doctor (Id, Speciality)
+select id, Speciality
+from @insertedDocIDt1;
 
 --------------------------------------------------------------------------------------------------
--- Insert InPatient
-declare @InsertedPatientIdTable3 table (Id int)
+--Insert records into Nurse table    
+DECLARE @insertedNurIDt2 TABLE (id INT)
+insert into Employee (EmpType, Fname, Lname, DOB, Phone, Gender, Street, City, State, DeptID)
+output inserted.Id into @insertedNurIDt2
+values
+	('N', 'Sara', 'Hassan', '1990-10-22', NULL, 'F', NULL, 'Giza', 'Egypt', 102),
+    ('N', 'Mona', 'Youssef', '1992-03-15', '0129876543', 'F', 'Street 3', default, default, 103),
+	('N', 'Hala', 'Youssef', '1991-12-19', '0109876543', 'F', 'Street 5', 'Alexandria', 'Egypt', 104),
+    ('N', 'Nour', 'Salem', '1989-05-11', '0111234567', 'F', 'Street 6', 'Giza', 'Egypt', 100),
+    ('N', 'Maha', 'Ezzat', '1994-03-22', '0123454321', 'F', 'Street 7', default, default, 101);
+
+INSERT INTO Nurse (Id)
+SELECT id
+FROM @insertedNurIDt2;
+
+-----------------------------------------------------------------------------------------------
+--Insert records into Receptionist table
+DECLARE @insertedRecIDt2 TABLE (id INT)
+insert into Employee (EmpType, Fname, Lname, DOB, Phone, Gender, Street, City, State, DeptID)
+output inserted.Id into @insertedRecIDt2
+values
+	('R', 'Ahmed', 'Adel', '1990-10-22', NULL, 'M', NULL, default, default, 105),
+    ('R', 'Osama', 'Ali', '1992-03-15', '0129876543', 'M', 'Street 13', 'Alexandria', 'Egypt', 105),
+	('R', 'Hany', 'Hassan', '1991-12-19', '0109876543', 'M', 'Street 55', 'Giza', 'Egypt', 105)
+
+INSERT INTO Receptionist(Id)
+SELECT id
+FROM @insertedRecIDt2;
+
+--------------------------------------------------------------------------------------------------
+--Insert records into Room table
+insert into Room (Capacity, NId)
+values
+  (2, 6), (1, 7), (3, 8), (4, 9), (1, 10)
+
+--------------------------------------------------------------------------------------------------
+--Insert records into OutPatient table
+declare @InsertedOutPatientIdTable1 table (Id int)
 
 insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
-output inserted.Id into @InsertedPatientIdTable3
-values ('I', 'Ali', 'Omar', '1995-11-05', 'M', 'Park Lane', 'Giza', 'Egypt')
-
-declare @InsertedPatientId3 int
-select @InsertedPatientId3 = Id from @InsertedPatientIdTable3
-
-insert into InPatient (Id, RoNum)
-values (@InsertedPatientId3, 102)
-
---------------------------------------------------------------------------------------------------
--- Insert InPatient
-declare @InsertedPatientIdTable4 table (Id int)
-
-insert into Patient (PType, FName, Gender)
-output inserted.Id into @InsertedPatientIdTable4
-values ('I', 'Fatma', 'F')
-
-declare @InsertedPatientId4 int
-select @InsertedPatientId4 = Id from @InsertedPatientIdTable4
-
-insert into InPatient (Id)
-values (@InsertedPatientId4)
-
---------------------------------------------------------------------------------------------------
--- Insert OutPatient
-declare @InsertedPatientIdTable5 table (Id int)
-
-insert into Patient (FName, Gender, DOB)
-output inserted.Id into @InsertedPatientIdTable5
-values ('Mahmoud', 'M', '2005-07-07')
-
-declare @InsertedPatientId5 int
-select @InsertedPatientId5 = Id from @InsertedPatientIdTable5
+output inserted.Id into @InsertedOutPatientIdTable1
+values 
+	('O', 'Ali', 'Omar', '1995-11-05', 'M', 'Park Lane', 'Giza', 'Egypt'),
+	('O','Ahmed', 'Mohamed', '2000-05-20', 'M', 'Broadway', default, default),
+	('O','Mahmoud', null, '2005-07-07', 'M', null, default, default),
+	('O', 'Sara', null, '1990-07-08', 'F', null, default, default),
+	('O', 'Mariam', null, '2004-05-09', 'F', 'Maadi', default, default)
 
 insert into OutPatient (Id)
-values (@InsertedPatientId5)
+select id from @InsertedOutPatientIdTable1
 
 --------------------------------------------------------------------------------------------------
--- Insert Appointment
-insert into Appointment (Date, Note, DId, PId, RId)
-values ('2024-12-29 10:30:00', 'Follow-up check', 0, 1, 0)
+--Insert records into InPatient table
+--First patient
+declare @InsertedInPatientIdTable1 table (Id int)
 
-insert into Appointment (Note, DId, PId, RId)
-values ('Routine visit', 0, 2, 0);
+insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
+output inserted.Id into @InsertedInPatientIdTable1
+values ('I', 'Fatma', 'Ahmed', '1997-12-03', 'F', null, 'Giza', 'Egypt')
+
+declare @InsertedInPatientId1 int
+select @InsertedInPatientId1 = Id from @InsertedInPatientIdTable1
+
+insert into InPatient (Id, RoNum)
+values (@InsertedInPatientId1, 100)
+
+--------------------------------------------------------------------------------------------------
+--Second patient
+declare @InsertedInPatientIdTable2 table (Id int)
+
+insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
+output inserted.Id into @InsertedInPatientIdTable2
+values ('I', 'Hana', 'Khaled', '1998-06-15', 'F', 'Talaat Harb St', 'Cairo', 'Egypt');
+
+declare @InsertedInPatientId2 int
+select @InsertedInPatientId2 = Id from @InsertedInPatientIdTable2
+
+insert into InPatient (Id, RoNum)
+values (@InsertedInPatientId2, 101)
+
+--------------------------------------------------------------------------------------------------
+--Third patient
+declare @InsertedInPatientIdTable3 table (Id int)
+
+insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
+output inserted.Id into @InsertedInPatientIdTable3
+values ('I', 'Omar', 'Ibrahim', '1990-03-12', 'M', 'Corniche Rd', 'Alexandria', 'Egypt');
+
+declare @InsertedInPatientId3 int
+select @InsertedInPatientId3 = Id from @InsertedInPatientIdTable3
+
+insert into InPatient (Id, RoNum)
+values (@InsertedInPatientId3, 102)
+
+--------------------------------------------------------------------------------------------------
+--Forth patient
+declare @InsertedInPatientIdTable4 table (Id int)
+
+insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
+output inserted.Id into @InsertedInPatientIdTable4
+values ('I', 'Youssef', 'Ali', '1992-04-22', 'M', 'Shubra Rd', 'Cairo', 'Egypt');
+
+declare @InsertedInPatientId4 int
+select @InsertedInPatientId4 = Id from @InsertedInPatientIdTable4
+
+insert into InPatient (Id, RoNum)
+values (@InsertedInPatientId4, 103)
+
+--------------------------------------------------------------------------------------------------
+--Fifth patient
+declare @InsertedInPatientIdTable5 table (Id int)
+
+insert into Patient (PType, FName, LName, DOB, Gender, Street, City, State)
+output inserted.Id into @InsertedInPatientIdTable5
+values ('I', 'Salma', 'Hussein', '2001-10-08', 'F', 'Nasr St', 'Mansoura', 'Egypt');
+
+declare @InsertedInPatientId5 int
+select @InsertedInPatientId5 = Id from @InsertedInPatientIdTable5
+
+insert into InPatient (Id, RoNum)
+values (@InsertedInPatientId5, 104)
+
+--------------------------------------------------------------------------------------------------
+--Insert records into Appointment table
+insert into Appointment (Date, Note, DId, PId, RId)
+values 
+	('2024-12-29 5:30:00', 'Follow-up check', 3, 1, 12), ('2024-12-27 10:30:00', 'Follow-up', 1, 2, 12)
 
 insert into Appointment (Date, DId, PId, RId)
-values ('2024-12-30 4:00:00', 0, 3, 0)
+values
+	('2024-12-30 4:00:00', 2, 3, 11), ('2024-12-28 7:00:00', 5, 5, 11), ('2024-12-30 8:00:00', 1, 10, 12)
 
 insert into Appointment (DId, PId, RId)
-values (0, 4, 0);
+values	
+	(3, 4, 13), (4, 6, 13), (2, 7, 11)
 
 insert into Appointment (Note, DId, PId, RId)
-values ('Emergency Appointment',0, 5, 0)
+values 
+	('Routine visit', 1, 2, 12), ('Emergency Appointment', 1, 8, 13), ('Emergency Appointment', 4, 9, 11)
+
+--------------------------------------------------------------------------------------------------
+--Insert records into Drug table
+insert into Drug (Code, RecDosage) 
+values 
+	(120, '500mg Tablet every 4-6 hours'),
+	(121, '50mg Tablet 2-3 times a day'),
+	(122, '350mg Tablet every 4-6 hours'),
+	(123, '400mg Tablet every 4-6 hours'),
+	(124, '50mg Tablet 2-3 times a day')
+
+--------------------------------------------------------------------------------------------------
+--Insert records into DrugBrand table
+insert into DrugBrand (DrugCode, BrandName) 
+values 
+	(120, 'Panadol'), (121, 'Voltaren'), (122, 'Advil'),
+	(123, 'Aspirin'), (124, 'Cataflam')
+
+-- edit drugs
+
+--------------------------------------------------------------------------------------------------
+--Insert records into ExaminePatient table
+insert into ExaminePatient (DID, PID) 
+values 
+	(3, 1), (1, 2), (2, 3), (3, 4),(5, 5),
+	(4, 6), (2, 7), (1, 8), (4, 9), (1, 10)
+
+--------------------------------------------------------------------------------------------------
+--Insert records into Report table
+insert into Report (Disease, Symptom, Diagnosis, DID, PId)
+values
+  ('Flu', 'Fever', 'Influenza', 3, 4),
+  ('Cold', 'Sneezing', 'Common Cold', 2, 3),
+  ('Diabetes', 'Thirst', 'Type 2 Diabetes', 1, 2),
+  ('Hypertension', 'Dizziness', 'High Blood Pressure', 3, 1),
+  ('Migraine', 'Headache', 'Severe Migraine', 2, 5)
+
+-- insert for in - edit out
+
+--------------------------------------------------------------------------------------------------
+--Insert records into Prescription table
+insert into Prescription (DID, PID)
+values
+	(3, 1), (1, 2), (2, 3), (3, 4),(5, 5),
+	(4, 6), (2, 7), (1, 8), (4, 9), (1, 10)
+
+--------------------------------------------------------------------------------------------------
+--Insert records into PrescriptionDrug table
+insert into PrescriptionDrug (PreID, DrugCode, Dosage) 
+values 
+(10, 120, '500mg Tablet every 4-6 hours'),
+(11, 124, '50mg Tablet 2-3 times a day'),
+(12, 120, '350mg Tablet every 4-6 hours'), 
+(13, 123, '400mg Tablet every 4-6 hours'), 
+(14, 123, '50mg Tablet 2-3 times a day')
+
+-- insert for in - edit out 
+ 
+--------------------------------------------------------------------------------------------------
+--Insert records into Bill table
+insert into Bill (Amount, PId, RID)
+values
+  (150, 1, 11),
+  (200, 2, 12),
+  (300, 5, 12),
+  (400, 3, 11),
+  (500, 4, 13)
+
+-- insert for in
+
+--------------------------------------------------------------------------------------------------
+--Insert records into GivenDrug table
+insert into GivenDrug (Dosage, GivenDate, DrugCode, Inpid, NID) 
+values 
+('500mg tablet', '2024-12-01', 1, 101, 201)
+('50mg tablet', '2024-12-01', 2, 102, 202);
+('500mg tablet', '2024-12-02', 1, 101, 201);
+('350mg tablet', '2024-12-02', 3, 105, 205);
+('400mg tablet', '2024-12-03', 4, 104, 204);
+('50mg tablet ', '2024-12-04', 5, 105, 205);
 
